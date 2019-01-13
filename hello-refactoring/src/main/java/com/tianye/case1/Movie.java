5db3b6a9c2ac152121a0f41d54787f9cb65047aa
tianye1604 <tianye1604@163.com>
2019-01-13 23:09:14 +0800

    add study refactoring module

---
+++ hello-refactoring/src/main/java/com/tianye/case1/Movie.java
@@ -0,0 +1,32 @@
+package com.tianye.case1;
+
+/**
+ * 一个简单的纯数据类
+ */
+public class Movie {
+
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+
+    private String _title;
+    private int _priceCode;
+
+    public Movie(String _title, int _priceCode) {
+        this._title = _title;
+        this._priceCode = _priceCode;
+    }
+
+    public String getitle() {
+        return _title;
+    }
+
+    public void setTitle(String _title) {
+        this._title = _title;
+    }
+
+    public int getPriceCode() {
+        return _priceCode;
+    }
+
+}
---
+++ hello-refactoring/src/main/java/com/tianye/case1/Rental.java
@@ -0,0 +1,23 @@
+package com.tianye.case1;
+
+/**
+ * 表示某个顾客租了一部影片
+ */
+public class Rental {
+
+    private Movie _movie;
+    private int _daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this._movie = movie;
+        this._daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+
+    public int getDaysRented() {
+        return _daysRented;
+    }
+}
