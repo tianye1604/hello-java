74f401731944bc6881364a1f3533a9325421651d
tianye1604 <tianye1604@163.com>
2019-01-26 16:13:13 +0800

    MOVE METHOD

--- hello-refactoring/src/main/java/com/tianye/case1/Customer.java
+++ hello-refactoring/src/main/java/com/tianye/case1/Customer.java
@@ -32,7 +32,7 @@ public class Customer {
             Rental each = (Rental)rentals.nextElement();
 
             //determine amounts for each line
-            thisAmount = amountFor(each);
+            thisAmount = each.getCharge();
 
             //add frequent renter points
             frequentRenterPoints++ ;
@@ -54,34 +54,4 @@ public class Customer {
         return result;
     }
 
-    /**
-     * 提取函数步骤：
-     * 1、找出代码的逻辑泥团，并运用Extact Method
-     * 2、找出这段逻辑泥团代码中的局部变量和参数
-     * 3、任何不被修改的变量都可以被当成参数传入新的函数
-     * 4、如果只有一个变量会被修改，可以把它当作返回值
-     * @param aRental
-     * @return
-     */
-    private double amountFor(Rental aRental) {
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if(aRental.getDaysRented() > 2){
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if(aRental.getDaysRented() > 3){
-                    result += (aRental.getDaysRented()-3) * 1.5;
-                }
-                break;
-        }
-        return result;
-    }
 }
--- hello-refactoring/src/main/java/com/tianye/case1/Rental.java
+++ hello-refactoring/src/main/java/com/tianye/case1/Rental.java
@@ -20,4 +20,41 @@ public class Rental {
     public int getDaysRented() {
         return _daysRented;
     }
+
+
+    /**
+     * 提取函数 Extact Method：
+     * 1、找出代码的逻辑泥团，并运用Extact Method
+     * 2、找出这段逻辑泥团代码中的局部变量和参数
+     * 3、任何不被修改的变量都可以被当成参数传入新的函数
+     * 4、如果只有一个变量会被修改，可以把它当作返回值
+     * @return
+     */
+    /**
+     * Move Method
+     * 1、发现这个函数使用了来自Rental类的信息，却没有使用来自Customer类的信息
+     * 2、函数应该放在它所使用的数据的所属对象内，所以amountFor()应该移到Rental类去
+     * @return
+     */
+    double getCharge() {
+        double result = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if(getDaysRented() > 2){
+                    result += (getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if(getDaysRented() > 3){
+                    result += (getDaysRented()-3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
 }
